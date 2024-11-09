1e5f036ee47e344e17f4e50d9f40e5c6184bf1e9
mario <mario.escudero@alumnos.upm.es>
2024-11-09 17:04:46 +0100

    Cambiados los nombres del testing del constructor en AlienTest

--- src/test/java/AlienTest.java
+++ src/test/java/AlienTest.java
@@ -5,55 +5,55 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AlienTest {
     Alien alien;
     @org.junit.jupiter.api.Test
-    void determinarTipoCaso1(){
+    void Constructor1(){
         alien = new Alien(0,150);
         assertEquals(0,alien.getX());
         assertEquals(150,alien.getY());
     }
     @org.junit.jupiter.api.Test
-    void determinarTipoCaso2(){
+    void Constructor2(){
         alien = new Alien(1,150);
         assertEquals(1,alien.getX());
         assertEquals(150,alien.getY());
     }
     @org.junit.jupiter.api.Test
-    void determinarTipoCaso3(){
+    void Constructor3(){
         alien = new Alien(150,150);
         assertEquals(150,alien.getX());
         assertEquals(150,alien.getY());
     }
     @org.junit.jupiter.api.Test
-    void determinarTipoCaso4(){
+    void Constructor4(){
         alien = new Alien(150,0);
         assertEquals(150,alien.getX());
         assertEquals(0,alien.getY());
     }
     @org.junit.jupiter.api.Test
-    void determinarTipoCaso5(){
+    void Constructor5(){
         alien = new Alien(150,1);
         assertEquals(150,alien.getX());
         assertEquals(1,alien.getY());
     }
     @org.junit.jupiter.api.Test
-    void determinarTipoCaso6(){
+    void Constructor6(){
         alien = new Alien(358,150);
         assertEquals(358,alien.getX());
         assertEquals(150,alien.getY());
     }
     @org.junit.jupiter.api.Test
-    void determinarTipoCaso7(){
+    void Constructor7(){
         alien = new Alien(357,150);
         assertEquals(357,alien.getX());
         assertEquals(150,alien.getY());
     }
     @org.junit.jupiter.api.Test
-    void determinarTipoCaso8(){
+    void Constructor8(){
         alien = new Alien(150,350);
         assertEquals(150,alien.getX());
         assertEquals(350,alien.getY());
     }
     @org.junit.jupiter.api.Test
-    void determinarTipoCaso9(){
+    void Constructor9(){
         alien = new Alien(150,349);
         assertEquals(150,alien.getX());
         assertEquals(349,alien.getY());
